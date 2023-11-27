2f0d08a7a9511b3ac5b8ae239135390113ddfd3f
Kovacs Ervin <kov.ervin97@gmail.com>
2023-11-27 11:32:18 +0200

    sync

--- src/app/boot/app-routing.module.ts
+++ src/app/boot/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from '../sp-common/views/not-found/not-found.component';
-import {AuthGuard} from '../auth/guards/auth.guard';
-import {UnauthenticatedGuard} from '../auth/guards/unauthenticated.guard';
+import { AuthGuard } from '../auth/guards/auth.guard';
+import { UnauthenticatedGuard } from '../auth/guards/unauthenticated.guard';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'web' },
   { path: 'web', canActivate: [AuthGuard], loadChildren: () => import('../platform/platform.module').then(m => m.PlatformModule) },
   { path: 'auth', canActivate: [UnauthenticatedGuard], loadChildren: () => import('../auth/auth.module').then(m => m.AuthModule) },
-  { path: 'register', canActivate: [UnauthenticatedGuard], loadChildren: () => import('../register/register.module').then(m => m.RegisterModule)},
-  { path: 'pr', canActivate: [UnauthenticatedGuard], loadChildren: () => import('../password-reset/password-reset.module').then(m => m.PasswordResetModule)},
+  { path: 'register', canActivate: [UnauthenticatedGuard], loadChildren: () => import('../register/register.module').then(m => m.RegisterModule) },
+  { path: 'pr', canActivate: [UnauthenticatedGuard], loadChildren: () => import('../password-reset/password-reset.module').then(m => m.PasswordResetModule) },
   { path: '**', component: NotFoundComponent }
 ];
 
